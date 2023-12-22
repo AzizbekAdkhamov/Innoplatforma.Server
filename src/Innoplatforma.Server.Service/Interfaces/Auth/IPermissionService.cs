15307098e37d9deee5c781e2ad38f0d270c4b31f
Dilafruza <dilafruzjraboyeva@gmail.com>
2023-12-22 19:38:45 +0500

    PermissionService implemented, but there is a some mistakes

--- src/Innoplatforma.Server.Service/Interfaces/Auth/IPermissionService.cs
+++ src/Innoplatforma.Server.Service/Interfaces/Auth/IPermissionService.cs
@@ -1,6 +1,13 @@
-﻿namespace Innoplatforma.Server.Service.Interfaces.Auth;
+﻿using Innoplatforma.Server.Service.DTOs.Auth.Permissions;
 
-public interface IPermissionService
+namespace Innoplatforma.Server.Service.Interfaces.Auth;
+
+public interface IPermissionService 
 {
-    
+    Task<bool> RemoveAsync(int id);
+    Task<PermissionForResultDto> RetrieveByIdAsync(int id);
+    Task<PermissionForResultDto> CreateAsync(PermissionForResultDto dto);
+    Task<PermissionForResultDto> ModifyAsync(int id, PermissionForUpdateDto dto);
+
+    //Task<IEnumerable<PermissionForResultDto>> RetrieveAllAsync(PaginationParams @params);
 }
