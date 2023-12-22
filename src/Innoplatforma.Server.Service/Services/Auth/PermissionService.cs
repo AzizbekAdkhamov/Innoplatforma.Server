a7d181a6b07f0f9fec4e7bc63d6e0c7ea1bba03a
Dilafruza <dilafruzjraboyeva@gmail.com>
2023-12-22 20:49:59 +0500

    Pagination logic, to paged list and httpcontexthelper

--- src/Innoplatforma.Server.Service/Services/Auth/PermissionService.cs
+++ src/Innoplatforma.Server.Service/Services/Auth/PermissionService.cs
@@ -1,5 +1,95 @@
-﻿namespace Innoplatforma.Server.Service.Services.Auth;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Innoplatforma.Server.Domain.Entities.Auth;
+using Innoplatforma.Server.Service.Interfaces.Auth;
+using Innoplatforma.Server.Data.IRepositories.Auth;
+using Innoplatforma.Server.Service.DTOs.Auth.Permissions;
+using Innoplatforma.Server.Service.Exceptions;
+using Innoplatforma.Server.Service.Exceptions;
+
+namespace Innoplatforma.Server.Service.Services.Auth;
 
 public class PermissionService
 {
+    private readonly IMapper _mapper;
+    private readonly IPermissionRepository _permissionRepository;
+
+    public PermissionService(IMapper mapper, IPermissionRepository permissionRepository)
+    {
+        _mapper = mapper;
+        _permissionRepository = permissionRepository;
+    }
+
+    public async Task<PermissionForResultDto> CreateAsync(PermissionForCreationDto dto)
+    {
+        var permission = await _permissionRepository.SelectAll()
+                .Where(p => p.Name.ToLower() == dto.Name.ToLower())
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+
+        if (permission is not null)
+            throw new InnoPlatformException(409, "Permission is already exist.");
+
+        var mappedPermission = _mapper.Map<Permission>(dto);
+        mappedPermission.CreatedAt = DateTime.UtcNow;
+
+        var createdPermission = await _permissionRepository.InsertAsync(mappedPermission);
+
+        return _mapper.Map<PermissionForResultDto>(createdPermission);
+    }
+
+    public async Task<PermissionForResultDto> ModifyAsync(int id, PermissionForUpdateDto dto)
+    {
+        var permission = await _permissionRepository.SelectAll()
+                .Where(p => p.Id == id)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+
+        if (permission is null)
+            throw new Exception(404, "Permission is not found");
+
+        var mappedPermission = _mapper.Map(dto, permission);
+        mappedPermission.UpdatedAt = DateTime.UtcNow;
+
+        await _permissionRepository.UpdateAsync(mappedPermission);
+
+        return _mapper.Map<PermissionForResultDto>(mappedPermission);
+    }
+
+    public async Task<bool> RemoveAsync(int id)
+    {
+        var permission = await _permissionRepository.SelectAll()
+                .Where(p => p.Id == id)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+
+        if (permission is null)
+            throw new Exception(404, "Permission is not found");
+
+        return await _permissionRepository.DeleteAsync(id);
+    }
+
+    public async Task<IEnumerable<PermissionForResultDto>> RetrieveAllAsync(PaginationParams @params)
+    {
+        var languages = await _permissionRepository.SelectAll()
+            .AsNoTracking()
+            .ToPagedList<Permission, int>(@params)
+            .ToListAsync();
+
+        return _mapper.Map<IEnumerable<PermissionForResultDto>>(languages);
+    }
+
+    public async Task<PermissionForResultDto> RetrieveByIdAsync(short id)
+    {
+        var permission = await _permissionRepository.SelectAll()
+                .Where(p => p.Id == id)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+
+        if (permission is null)
+            throw new Exception(404, "Permission is not found");
+
+        return _mapper.Map<PermissionForResultDto>(permission);
+    }
+>>>>>>> Stashed changes
 }
