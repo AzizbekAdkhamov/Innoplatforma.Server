b4f97e315402e1458a77bac1c2de0ddd2469b150
Jaloliddin | Jm7Uz <87977243+jm7uz@users.noreply.github.com>
2023-12-23 11:19:46 +0500

    Implemented AccountService and EmailService

--- src/Innoplatforma.Server.Service/Services/Accounts/AccountService.cs
+++ src/Innoplatforma.Server.Service/Services/Accounts/AccountService.cs
@@ -1,11 +1,11 @@
-﻿using Innoplatforma.Server.Data.IRepositories;
+﻿using Microsoft.EntityFrameworkCore;
+using Innoplatforma.Server.Data.IRepositories;
+using Innoplatforma.Server.Service.Exceptions;
+using Innoplatforma.Server.Service.DTOs.Logins;
 using Innoplatforma.Server.Domain.Entities.Users;
 using Innoplatforma.Server.Service.Commons.Helpers;
-using Innoplatforma.Server.Service.DTOs.Logins;
-using Innoplatforma.Server.Service.Exceptions;
 using Innoplatforma.Server.Service.Interfaces.Accounts;
 using Innoplatforma.Server.Service.Interfaces.Commons;
-using Microsoft.EntityFrameworkCore;
 
 
 namespace Innoplatforma.Server.Service.Services.Accounts;
@@ -23,14 +23,17 @@ public class AccountService : IAccountService
     }
     public async Task<string> LoginAsync(LoginDto loginDto)
     {
-        throw new NotImplementedException();
-        /*var user = await userRepository.SelectAll()
+        var user = await userRepository.SelectAll()
                 .Where(a => a.Phone == loginDto.PhoneNumber)
                 .AsNoTracking()
                 .FirstOrDefaultAsync();
         if (user is null)
             throw new InnoplatformException(404, "Telefor raqam yoki parol xato kiritildi!");
 
-        var hasherResult = PasswordHelper.Verify(loginDto.Password, user.Salt, user.Password);*/
+        var hasherResult = PasswordHelper.Verify(loginDto.Password, user.Salt, user.Password);
+        if (hasherResult == false)
+            throw new InnoplatformException(404, "Telefor raqam yoki parol xato kiritildi!");
+
+        return authService.GenerateToken(user);
     }
 }
