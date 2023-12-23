d00662c1d7dcdcc4b9037db22df56f1a3667fdc4
Jaloliddin | Jm7Uz <87977243+jm7uz@users.noreply.github.com>
2023-12-23 10:15:15 +0500

    Implemented ApplicationService and InvestmentService

---
+++ src/Innoplatforma.Server.Service/Interfaces/Investments/IInvestmentService.cs
@@ -0,0 +1,14 @@
+﻿using Innoplatforma.Server.Service.Configurations;
+using Innoplatforma.Server.Service.DTOs.Applications;
+using Innoplatforma.Server.Service.DTOs.Investments;
+
+namespace Innoplatforma.Server.Service.Interfaces.Investments;
+
+public interface IInvestmentService
+{
+    Task<bool> RemoveAsync(long id);
+    Task<InvestmentForResultDto> RetrieveByIdAsync(long id);
+    Task<InvestmentForResultDto> AddAsync(InvestmentForCreateDto dto);
+    Task<InvestmentForResultDto> ModifyAsync(long id, InvestmentForUpdateDto dto);
+    Task<IEnumerable<InvestmentForResultDto>> RetrieveAllAsync(PaginationParams @params);
+}
---
+++ src/Innoplatforma.Server.Service/Services/Applications/ApplicationService.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Innoplatforma.Server.Data.IRepositories;
+using Innoplatforma.Server.Service.Exceptions;
+using Innoplatforma.Server.Service.Configurations;
+using Innoplatforma.Server.Service.Commons.Extentions;
+using Innoplatforma.Server.Service.DTOs.Applications;
+using Innoplatforma.Server.Domain.Entities.Applications;
+using Innoplatforma.Server.Service.Interfaces.Applications;
+
+namespace Innoplatforma.Server.Service.Services.Applications;
+
+public class ApplicationService : IApplicationService
+{
+    private readonly IMapper mapper;
+    private readonly IRepository<Application, long> applicationRepository;
+
+    public ApplicationService(
+        IMapper mapper,
+        IRepository<Application, long> applicationRepository)
+    {
+        this.mapper = mapper;
+        this.applicationRepository = applicationRepository; 
+    }
+
+    public async Task<ApplicationForResultDto> AddAsync(ApplicationForCreationDto dto)
+    {
+        var application = await applicationRepository.SelectAll()
+                .Where(a => a.Title.ToLower() == dto.Title.ToLower())
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+
+        if (application is not null)
+            throw new InnoplatformException(409, "Application alredy exists!");
+
+        var mapped = mapper.Map<Application>(dto);
+
+        var result = await applicationRepository.InsertAsync(mapped);
+
+        return mapper.Map<ApplicationForResultDto>(result);
+    }
+
+    public async Task<ApplicationForResultDto> ModifyAsync(long id, ApplicationForUpdateDto dto)
+    {
+        var application = await applicationRepository.SelectAll()
+            .Where(a => a.Id == id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+        if (application is null)
+            throw new InnoplatformException(404, "Application is not found!");
+
+        var mapped = mapper.Map(dto, application);
+        mapped.UpdatedAt = DateTime.UtcNow;
+
+        await applicationRepository.UpdateAsync(mapped);
+
+        return mapper.Map<ApplicationForResultDto>(mapped);
+    }
+
+    public async Task<bool> RemoveAsync(long id)
+    {
+        var application = await applicationRepository.SelectAll()
+            .Where(a => a.Id == id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+        if (application is null)
+            throw new InnoplatformException(404, "Application is not found!");
+
+        await applicationRepository.DeleteAsync(id);
+
+        return true;
+    }
+
+    public async Task<IEnumerable<ApplicationForResultDto>> RetrieveAllAsync(PaginationParams @params)
+    {
+        var application = await applicationRepository.SelectAll()
+            .AsNoTracking()
+            .ToPagedList<Application, long>(@params)
+            .ToListAsync();
+
+        return mapper.Map<IEnumerable<ApplicationForResultDto>>(application);
+    }
+
+    public async Task<ApplicationForResultDto> RetrieveByIdAsync(long id)
+    {
+        var application = await applicationRepository.SelectByIdAsync(id);
+
+        if (application is null)
+            throw new InnoplatformException(404, "Application is not found");
+
+        return mapper.Map<ApplicationForResultDto>(application);
+    }
+}
