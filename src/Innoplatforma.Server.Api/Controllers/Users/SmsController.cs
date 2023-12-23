c2b532daaba540f92dbb83d5e01d82d9ed85c988
Jaloliddin | Jm7Uz <87977243+jm7uz@users.noreply.github.com>
2023-12-23 18:26:31 +0500

    Sms Controller implemented

---
+++ src/Innoplatforma.Server.Api/Controllers/Users/SmsController.cs
@@ -0,0 +1,31 @@
+﻿using Innoplatforma.Server.Api.Controllers.Commons;
+using Innoplatforma.Server.Service.Interfaces.Users;
+using Innoplatforma.Server.Service.Services.Accounts.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Innoplatforma.Server.Api.Controllers.Users;
+
+public class SmsController : BaseController
+{
+    private readonly ISmsService smsService;
+
+    public SmsController(ISmsService smsService)
+    {
+        this.smsService = smsService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendMessageAsync(Message message)
+        => Ok(await this.smsService.SendAsync(message));
+
+
+    [HttpPost("send-message-by-telegram/groupId/url/text")]
+    public async Task<IActionResult> PostAsync(long groupId, string url, string text)
+    {
+        // Decode the URL parameter
+        url = Uri.UnescapeDataString(url);
+
+        return Ok(await smsService.SendMessageByTelegramAsync(groupId, url, text));
+    }
+}
