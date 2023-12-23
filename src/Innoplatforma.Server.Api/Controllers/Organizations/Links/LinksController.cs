9c5d747d8dd64b7e78aea35c9f31e30e460fd1cc
Dilafruza <dilafruzjraboyeva@gmail.com>
2023-12-23 11:00:33 +0500

    LinksController done

--- src/Innoplatforma.Server.Api/Controllers/Organizations/Links/LinksController.cs
+++ src/Innoplatforma.Server.Api/Controllers/Organizations/Links/LinksController.cs
@@ -1,8 +1,37 @@
 ﻿using Innoplatforma.Server.Api.Controllers.Commons;
+using Innoplatforma.Server.Service.Configurations;
+using Innoplatforma.Server.Service.DTOs.Organizations.Links;
+using Innoplatforma.Server.Service.Interfaces.Organizations.Links;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Innoplatforma.Server.Api.Controllers.Organizations.Links;
 
 public class LinksController : BaseController
 {
+    private readonly ILinkService _linkService;
 
+    public LinksController(ILinkService linkService)
+    {
+        _linkService = linkService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> InsertAsync([FromBody] LinkForCreationDto dto)
+        => Ok(await _linkService.CreateAsync(dto));
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync([FromQuery] PaginationParams @params)
+        => Ok(await _linkService.RetrieveAllAsync(@params));
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdAsync([FromRoute] long id)
+        => Ok(await _linkService.RetrieveByIdAsync(id));
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> RemoveAsync([FromRoute] long id)
+        => Ok(await _linkService.RemoveAsync(id));
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateAsync([FromRoute] long id, [FromBody] LinkForUpdateDto dto)
+        => Ok(await _linkService.ModifyAsync(id, dto));
 }
